615598f81ba79b8341442263baa5e36b42363e36
Giorgio Visinoni <pannellosolare7@gmail.com>
2018-02-19 09:25:28 +0100

    Aggiunto somma20 e somma100

--- src/Sommatore.java
+++ src/Sommatore.java
@@ -10,12 +10,29 @@ public class Sommatore
 	{
 		return x+10;
 	}
+	
+	public static int somma20(int x)
+	{
+		for (int i = 0; i < 20; i++) 
+			x++;
+		return x;
+	}
+	
+	public static int somma100(int x)
+	{
+		for (int i = 0; i < 100; i++) 
+			x++;
+		return x;
+	}
+	
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
 		int x=1;
 		System.out.println(somma4(x));
 		System.out.println(somma10(x));
+		System.out.println(somma20(x));
+		System.out.println(somma100(x));
 	}
 
 }
